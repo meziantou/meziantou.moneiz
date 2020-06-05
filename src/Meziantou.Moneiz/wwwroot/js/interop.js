01ceb3945ba522876d7692a62ba37242dec57b11
meziantou <meziantou@live.fr>
2020-06-04 21:05:03 -0400

    Fix download on safari

--- src/Meziantou.Moneiz/wwwroot/js/interop.js
+++ src/Meziantou.Moneiz/wwwroot/js/interop.js
@@ -1,39 +1,27 @@
-﻿function MoneizLoadDatabase() {
-  return localStorage.getItem("moneiz.db");
+﻿const MoneizLocalStorageName = "moneiz.db";
+const MoneizDbFileName = "moneiz.moneizdb";
+
+function MoneizLoadDatabase() {
+  return localStorage.getItem(MoneizLocalStorageName);
 }
 
 function MoneizSaveDatabase(content) {
-  return localStorage.setItem("moneiz.db", content);
+  return localStorage.setItem(MoneizLocalStorageName, content);
 }
 
-function MoneizExportDatabase(fileName) {
+function MoneizExportDatabase() {
   const content = MoneizLoadDatabase();
 
-  const file = new File([content], fileName, { type: "application/octet-stream" });
+  const file = new File([content], MoneizDbFileName, { type: "application/octet-stream" });
   const exportUrl = URL.createObjectURL(file);
 
   const a = document.createElement("a");
   document.body.appendChild(a);
-  a.href = exportUrl
-  a.download = fileName;
+  a.href = exportUrl;
+  a.download = MoneizDbFileName;
+  a.target = "_self";
   a.click();
-
-  URL.revokeObjectURL(exportUrl);
-}
-
-function MoneizImportDatabase(fileName) {
-  const content = MoneizLoadDatabase();
-
-  const file = new File([content], fileName, { type: "application/octet-stream" });
-  const exportUrl = URL.createObjectURL(file);
-
-  const a = document.createElement("a");
-  document.body.appendChild(a);
-  a.href = exportUrl
-  a.download = fileName;
-  a.click();
-
-  URL.revokeObjectURL(exportUrl);
+  //URL.revokeObjectURL(exportUrl); doesn't work with Safari
 }
 
 function MoneizConfirm(message) {
