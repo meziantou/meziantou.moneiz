2b30fb596da5c9328cb1d2a19c365544703385e1
meziantou <meziantou@live.fr>
2020-06-24 22:34:42 -0400

    Prevent closing tab if unexported changes

--- src/Meziantou.Moneiz/wwwroot/js/interop.js
+++ src/Meziantou.Moneiz/wwwroot/js/interop.js
@@ -73,3 +73,22 @@ function MoneizAlert(message) {
 function MoneizOpenInTab(url) {
   window.open(url, '_blank');
 }
+
+(function () {
+  let state = false;
+
+  window.addEventListener("beforeunload", function (e) {
+    if (state) {
+      e.returnValue = "You have unsaved changes. Do you want to exit before exporting your data?";
+    }
+  });
+
+  setInterval(async () => {
+    const value = await MoneizGetValue(MoneizLocalStorageChangedName);
+    if (value) {
+      state = JSON.parse(value);
+    } else {
+      state = false;
+    }
+  }, 500);
+})()
