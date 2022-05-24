d2f8a138db03f843b5fef20dfe09d75e9c7c2182
Gérald Barré <meziantou@live.fr>
2022-05-24 08:53:17 -0400

    Search for transactions of specific amount

---
+++ src/Meziantou.Moneiz/Extensions/AmountUtilities.cs
@@ -0,0 +1,9 @@
+﻿namespace Meziantou.Moneiz.Extensions;
+
+internal static class AmountUtilities
+{
+    public static string FormatAmount(decimal value)
+    {
+        return value.ToString("N2", System.Globalization.CultureInfo.InvariantCulture);
+    }
+}
